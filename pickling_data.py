e4b0580a3bee11e56efd7003da11875cff8088ad
Lokesh-Balla <Lokesh-Balla@users.noreply.github.com>
2019-10-10 16:19:02 +0530

    few changes

--- pickling_data.py
+++ pickling_data.py
@@ -9,7 +9,7 @@ training_fruit_img = []
 training_label = []
 for dir_path in glob.glob("Training/*"):
     img_label = dir_path.split("/")[-1]
-    for img_path in glob.glob(os.path.join(dir_path, "*.jpg")):
+    for img_path in glob.glob(os.path.join(dir_path, "*.jpeg")):
         img = cv2.imread(img_path)
         img = cv2.resize(img, (64, 64))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -30,7 +30,7 @@ test_fruit_img = []
 test_label = []
 for dir_path in glob.glob("Test/*"):
     img_label = dir_path.split("/")[-1]
-    for img_path in glob.glob(os.path.join(dir_path, "*.jpg")):
+    for img_path in glob.glob(os.path.join(dir_path, "*.jpeg")):
         img = cv2.imread(img_path)
         img = cv2.resize(img, (64, 64))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -46,22 +46,3 @@ test_fruit_img_file.close()
 test_label_file = open('test_label_file', 'wb')
 pickle.dump(test_label, test_label_file)
 test_label_file.close()
-#
-# test_fruits_img = []
-# tests_label = []
-# for img_path in glob.glob(os.path.join("test-multiple_fruits", "*.jpg")):
-#     img = cv2.imread(img_path)
-#     img = cv2.resize(img, (64, 64))
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     test_fruits_img.append(img)
-#     tests_label.append(img_label)
-# test_fruits_img = np.array(test_fruits_img)
-# tests_label = np.array(tests_label)
-#
-# test_fruits_img_file = open('test_fruits_img_file', 'wb')
-# pickle.dump(test_fruits_img, test_fruits_img_file)
-# test_fruits_img_file.close()
-#
-# tests_label_file = open('tests_label_file', 'wb')
-# pickle.dump(tests_label, tests_label_file)
-# tests_label_file.close()
--- train_and_test.py
+++ train_and_test.py
@@ -1,8 +1,7 @@
 import pickle
 
 import numpy as np
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
+import tensorflow as tf
 
 training_fruit_img_file = open('training_fruit_img_file', 'rb')
 training_label_file = open('training_label_file', 'rb')
@@ -18,14 +17,7 @@ test_label = pickle.load(test_label_file)
 test_fruit_img_file.close()
 test_label_file.close()
 
-test_fruits_img_file = open('test_fruits_img_file', 'rb')
-tests_label_file = open('tests_label_file', 'rb')
-test_fruits_img = pickle.load(test_fruits_img_file)
-tests_label = pickle.load(tests_label_file)
-test_fruits_img_file.close()
-tests_label_file.close()
-
-print("milestone 1")
+print("loading in pickled data complete")
 
 label_to_id = {v: k for k, v in enumerate(np.unique(training_label))}
 id_to_label = {v: k for k, v in label_to_id.items()}
@@ -38,35 +30,35 @@ id_to_label_file = open('id_to_label_file', 'wb')
 pickle.dump(id_to_label, id_to_label_file)
 id_to_label_file.close()
 
-print("milestone 2")
-
 training_label_id = np.array([label_to_id[i] for i in training_label])
 test_label_id = np.array([label_to_id[i] for i in test_label])
 
-print("milestone 3")
+print("labels ready and pickled")
 
 training_fruit_img, test_fruit_img = training_fruit_img / 255.0, test_fruit_img / 255.0
 
+print("values normalized")
+
 # Actual Model
-model = Sequential()
-model.add(Conv2D(16, (3, 3), input_shape=(64, 64, 3), padding="same", activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(16, (3, 3), padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
-model.add(Dense(256, activation="relu"))
-model.add(Dense(75, activation="softmax"))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation="relu"))
+model.add(tf.keras.layers.Dense(75, activation="softmax"))
 
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
-model.fit(training_fruit_img, training_label_id, batch_size=32, epochs=1)
+model.fit(training_fruit_img, training_label_id, batch_size=32, epochs=15)
 
 loss, accuracy = model.evaluate(test_fruit_img, test_label_id)
 print("Loss:", loss)
